319cb2a47cac098feea70d877d94f660d6c8f9be
wujiahua <wujiahua_lisp@outlook.com>
2022-05-29 23:21:06 +0800

    enable ocaml-mode

---
+++ lisp/init-ocaml.el
@@ -0,0 +1,6 @@
+;;; init-ocaml  ---  define ocaml  configuration
+;;; Commentary:
+;;; Code:
+(require 'opam-user-setup "~/.emacs.d/lisp/opam-user-setup.el")
+(provide 'init-ocaml)
+;;; init-ocaml.el ends here
