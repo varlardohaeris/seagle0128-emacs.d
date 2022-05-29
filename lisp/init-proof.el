2cf6690a215b3f0a915c37b6fc0c136a713946a4
wujiahua <wujiahua_lisp@outlook.com>
2022-05-29 23:15:14 +0800

    add PG for coq

---
+++ lisp/init-proof.el
@@ -0,0 +1,9 @@
+;;; init-proof ---  define proof for coq, etc
+;;; Commentary:
+;;; Code:
+
+(load "~/.emacs.d/local/PG/generic/proof-site")
+(setq coq-prog-name "/usr/local/bin/coqtop")
+
+(provide 'init-proof)
+;;; init-proof.el ends here
